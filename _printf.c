93d3b7d87a34349610e5907462a9fa9efb7b350a
aalaaimam <aalaaimam@gmail.com>
2023-08-20 06:51:23 -0700

    updated

--- _printf.c
+++ _printf.c
@@ -1,8 +1,4 @@
-#include <stdio.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#define BUFF_SIZE 1024
-int _printf(const char *format, ...);
+#include "main.h"
 void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
@@ -52,3 +48,9 @@ return (printed_chars);
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
+void print_buffer(char buffer[], int *buff_ind)
+{
+if (*buff_ind > 0)
+write(1, &buffer[0], *buff_ind);
+*buff_ind = 0;
+}
