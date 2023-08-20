b33c8da7b803376a5a69d5ebbd5437e7fc687d4b
aalaaimam <aalaaimam@gmail.com>
2023-08-20 06:20:01 -0700

    updated

--- _printf.c
+++ _printf.c
@@ -1,44 +1,56 @@
-#include <stdio.h>
-#include <stdarg.h>
+#include "main.h"
+void print_buffer(char buffer[], int *buff_ind);
 /**
- * _printf - Format and print data to stdout
- * @format: A string containing zero or more format specifiers
- *
- * Return: The number of characters printed
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-va_list args;
-int count = 0;
-char c;
-va_start(args, format);
-while ((c = *format++) != '\0')
+int i, printed = 0, printed_chars = 0;
+int flags, width, precision, size, buff_ind = 0;
+va_list list;
+char buffer[BUFF_SIZE];
+if (format == NULL)
+return (-1);
+va_start(list, format);
+for (i = 0; format && format[i] != '\0'; i++)
 {
-if (c == '%')
+if (format[i] != '%')
 {
-switch (*format++)
-{
-case 'c':
-putchar(va_arg(args, int));
-count++;
-break;
-case 's':
-count += printf("%s", va_arg(args, char *));
-break;
-case '%':
-putchar('%');
-count++;
-break;
-default:
-break;
-}
+buffer[buff_ind++] = format[i];
+if (buff_ind == BUFF_SIZE)
+print_buffer(buffer, &buff_ind);
+/* write(1, &format[i], 1);*/
+printed_chars++;
 }
 else
 {
-putchar(c);
-count++;
+print_buffer(buffer, &buff_ind);
+flags = get_flags(format, &i);
+width = get_width(format, &i, list);
+precision = get_precision(format, &i, list);
+size = get_size(format, &i);
+++i;
+printed = handle_print(format, &i, list, buffer,
+flags, width, precision, size);
+if (printed == -1)
+return (-1);
+printed_chars += printed;
 }
 }
-va_end(args);
-return (count);
+print_buffer(buffer, &buff_ind);
+va_end(list);
+return (printed_chars);
+}
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+if (*buff_ind > 0)
+write(1, &buffer[0], *buff_ind);
+*buff_ind = 0;
 }
